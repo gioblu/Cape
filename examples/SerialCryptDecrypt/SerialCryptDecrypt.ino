cbc47b483c991794202ad42a0261de09567287ce
unknown <gioscarab@gmail.com>
2015-10-11 13:19:31 +0200

    Example, 1.5x compatibility, keywords and properties

---
+++ examples/SerialCryptDecrypt/SerialCryptDecrypt.ino
@@ -0,0 +1,23 @@
+#include <Cape.h>
+
+Cape cape("Hello world!", 2, true);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  cape.encrypt("Hello world!", 12);
+  for(int i = 0; i < MAX_LENGTH; i++)
+    Serial.print(cape.result[i]);
+  Serial.println();
+  
+  cape.decrypt(cape.result, 12);
+    for(int i = 0; i < MAX_LENGTH; i++)
+    Serial.print(cape.result[i]);
+  Serial.println();
+  
+  delay(100);
+}
