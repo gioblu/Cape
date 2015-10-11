2266cf3a50b816db01e318c0660006b90819af8f
unknown <gioscarab@gmail.com>
2015-10-11 13:20:53 +0200

    Refactoring plus keyworda

--- Cape.h
+++ Cape.h
@@ -6,27 +6,18 @@
 
 #ifndef Cape_h
   #define Cape_h
-
-  #if defined(ARDUINO) && (ARDUINO >= 100)
-    #include "Arduino.h"
-  #else
-    #include "WProgram.h"
-    #include "WConstants.h"
-  #endif
+  #include "Arduino.h"
 #endif
 
 #define swap(a,b) do { int t = _s_box[a]; _s_box[a] = _s_box[b]; _s_box[b] = t; } while(0)
 #define MAX_LENGTH 100
 
 class Cape {
-
   public:
-
     Cape(char *encryption_key, uint8_t strength, boolean initialization_vector = false);
     void crypt(char *data, uint8_t length, boolean initialization_vector = false, boolean side = false);
     void encrypt(char *data, uint8_t length);
     void decrypt(char *data, uint8_t length);
-
     uint8_t generate_IV();
     char result[MAX_LENGTH];
 
