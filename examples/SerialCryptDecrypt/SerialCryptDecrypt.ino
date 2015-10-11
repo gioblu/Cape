844928fe2a4fd48eedd7d3d3c274d50666fadbd9
Giovanni Blu Mitolo <gioscarab@gmail.com>
2015-10-11 13:29:18 +0200

    Update SerialCryptDecrypt.ino

--- examples/SerialCryptDecrypt/SerialCryptDecrypt.ino
+++ examples/SerialCryptDecrypt/SerialCryptDecrypt.ino
@@ -3,12 +3,11 @@
 Cape cape("YOUR-PRIVATE-KEY", 2, true);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   cape.encrypt("Hello world!", 12);
   for(int i = 0; i < MAX_LENGTH; i++)
     Serial.print(cape.result[i]);
