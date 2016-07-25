f30b48478d8ea6760464b35cdd050c31afdcd4a9
unknown <gioscarab@gmail.com>
2016-07-25 17:40:03 +0200

    set_key method added

--- Cape.cpp
+++ Cape.cpp
@@ -2,51 +2,49 @@
    |      |     | |     | |
    |      |_____| |_____| |_____
    |      |     | |       |
-   |_____ |     | |       |_____  version 1.2
+   |_____ |     | |       |_____  version 1.4
 
 Cape Copyright (c) 2012-2016, Giovanni Blu Mitolo All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-- Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
--  Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
+    http://www.apache.org/licenses/LICENSE-2.0
 
--  All advertising materials mentioning features or use of this software
-   must display the following acknowledgement:
-   "Secured by Cape encryption, Giovanni Blu Mitolo 2012-2016"
-
--  Neither the name of Cape nor the names of its contributors
-   may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-This software is provided by the copyright holders and contributors "as is"
-and any express or implied warranties, including, but not limited to, the
-implied warranties of merchantability and fitness for a particular purpose
-are disclaimed. In no event shall the copyright holder or contributors be liable
-for any direct, indirect, incidental, special, exemplary, or consequential
-damages (including, but not limited to, procurement of substitute goods or
-services; loss of use, data, or profits; or business interruption) however
-caused and on any theory of liability, whether in contract, strict liability,
-or tort (including negligence or otherwise) arising in any way out of the use
-of this software, even if advised of the possibility of such damage. */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include "Cape.h"
 
 /* Initiate Cape passing a random string (maximum 255 characters), the number
    of hashing iterations you want to perform (max 32767) */
 
-Cape::Cape(char *key, uint8_t key_length) {
+Cape::Cape(char *key, uint8_t length) {
   _key = key;
-  _key_length = key_length;
+  _key_length = length;
+  compute_reduced_key(key, length);
+};
 
-  // Generate a 1 byte version of the private key
-  for(uint8_t i = 0; i < strlen(_key); i++)
-    _reduced_key ^= key[i];
-}
+
+/* Compute a 1 byte version of the private key: */
+
+void Cape::compute_reduced_key(char *key, uint8_t length) {
+  for(uint8_t i = 0; i < length; i++)
+     _reduced_key ^= key[i];
+};
+
+
+/* Set or Change encryption key: */
+
+void Cape::set_key(char *key, uint8_t length) {
+  _key = key;
+  _key_length = length;
+  compute_reduced_key(key, length);
+};
 
 
 /* Private key, stream chipher algorithm with masked initialization vector */
@@ -81,8 +79,7 @@ void Cape::crypt(char *source, char *destination, uint8_t length, boolean initia
       destination[i] ^= source[length];
   }
 
-  // Hash data with key
-  // Static, reversible hashing
+  // Hash data with key (static reversible hashing)
   for (i = 0; i < length; i++)
     destination[i] = source[i] ^ _key[i % _key_length];
 
@@ -103,6 +100,7 @@ void Cape::crypt(char *source, char *destination, uint8_t length, boolean initia
       destination[i] ^= (i ^ _key[(i ^ _reduced_key) % _key_length]);
   }
 };
+
 
 uint8_t Cape::generate_IV() {
   return (random(0, 256) * millis()) % 255 + 1;
