091b293993efda4600cc6cb2fceb89f9bc59d5bb
unknown <gioscarab@gmail.com>
2017-08-02 18:28:56 +0200

    SerialCryptDecrypt example updated SerialHash added

--- examples/SerialHash/SerialHash.ino
+++ examples/SerialHash/SerialHash.ino
@@ -6,10 +6,9 @@ char key[] = "9LKSUR&#5A";
 // Source content to be crypted
 char source[] = "HELLOWORLD";
 // Result buffer needs an additional byte for the initialization vector
-char destination[11];
+char destination[10];
 // Insert secret key and its length
 Cape cape(key, 10);
-
 
 void setup() {
   Serial.begin(115200);
@@ -17,55 +16,42 @@ void setup() {
 
 void loop() {
   Serial.print("Encryption key: ");
-
   // Generate a random key
   for(int i = 0; i < 10; i++) {
    key[i] = (uint8_t)random(0, 127);
    Serial.print(key[i]);
   }
-
   Serial.println();
-
   // Set it as the encryption key
   cape.set_key(key, 10);
-
+  // Hash and measure duration
   unsigned long time = micros();
-  cape.encrypt(source, destination, 10, random(0, 255));
+  cape.hash(source, destination, 10);
   time = micros() - time;
-
   // Destroy original content
   for(int i = 0; i < 10; i++) source[i] = 0;
-
-  Serial.print("CRIPTED:  ");
+  // Output result
+  Serial.print("ENCRYPTED:  ");
   for(int i = 0; i < 10; i++)
     Serial.print(destination[i]);
-
-  Serial.print(" Masked IV: ");
-  Serial.print((uint8_t)destination[11], DEC);
-
   Serial.print(" Computation time: ");
   Serial.print(time);
   Serial.println(" microseconds");
-
+  // Hash to get back original string
   time = micros();
-  cape.decrypt(destination, source, 11);
+  cape.hash(destination, source, 10);
   // Result is 1 byte longer to contain the added initialization vector
   time = micros() - time;
-
-  Serial.print("ORIGINAL: ");
+  // Output result
+  Serial.print("ORIGINAL:   ");
   for(int i = 0; i < 10; i++)
     Serial.print(source[i]);
-
-  Serial.print("   Real IV: ");
-  Serial.print((uint8_t)source[11], DEC);
-
   Serial.print(" Computation time: ");
   Serial.print(time);
   Serial.print(" microseconds");
-
   // Erase buffer
   for(int i = 0; i < 10; i++) destination[i] = 0;
-
+  // Delay
   Serial.println();
   Serial.println();
   delay(250);
